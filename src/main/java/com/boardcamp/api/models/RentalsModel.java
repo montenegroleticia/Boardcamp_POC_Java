ad09e2f894f94bdd3bcac11e55559018d1c6029b
montenegroleticia <montenegro.letiicia@gmail.com>
2024-02-08 06:57:31 -0300

    feat: ManyToOne relation

--- src/main/java/com/boardcamp/api/models/RentalsModel.java
+++ src/main/java/com/boardcamp/api/models/RentalsModel.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -40,4 +42,12 @@ public class RentalsModel {
 
     @Column(nullable = false)
     private Integer delayFee;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId")
+    private CustomersModel customer;
+
+    @ManyToOne
+    @JoinColumn(name = "gameId")
+    private GameModel game;
 }
